da78333ad645aebf5fa5c22cd5bc97ae0410f7a9
libregd <libregd@outlook.com>
2025-09-11 20:38:54 +0800

    ver 0.1
    - Added a custom logo  ,banner
    - Removed unused features from the original template
    - Customized the left sidebar for personal use
    - Switched to English as the primary language

---
+++ README.md
@@ -0,0 +1,14 @@
+![banner](./files/banner.png)
+# Background
+
+The project started from frequently coming across interesting things—I call them "light," or maybe "sparks." 
+
+It's not a tool-focused list of recommendations, but something deeper. I'm still figuring out how best to begin.
+
+
+# Credits
+  
+This site is built using the Astro [astro-docs-template](https://github.com/advanced-astro/astro-docs-template)  template, with many features removed—mainly because it's not meant for writing open-source technical documentation, but for hosting a personal collection of sparks.
+
+
+
--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 export const SITE = {
-  title: 'Documentation',
+  title: 'Pick up the light',
   description: 'Your website description.',
   defaultLanguage: 'en-us'
 } as const
@@ -35,13 +35,38 @@ export type Sidebar = Record<
   (typeof KNOWN_LANGUAGE_CODES)[number],
   Record<string, { text: string; link: string }[]>
 >
+// export const SIDEBAR: Sidebar = {
+//   en: {
+//     'creation': [
+//       { text: 'Introduction', link: 'en/introduction' },
+//       { text: 'Page 2', link: 'en/page-2' },
+//       { text: 'Page 3', link: 'en/page-3' }
+//     ],
+//     'Another Section': [{ text: 'Page 4', link: 'en/page-4' }]
+//   }
+// }
 export const SIDEBAR: Sidebar = {
-  cn: {
-    'Section Header': [
-      { text: 'Introduction', link: 'cn/introduction' },
-      { text: 'Page 2', link: 'cn/page-2' },
-      { text: 'Page 3', link: 'cn/page-3' }
-    ],
-    'Another Section': [{ text: 'Page 4', link: 'cn/page-4' }]
+  en: {
+    'Technology & Design': {
+      'Sparks': [
+        { text: 'sample 1', link: 'en/technology-design/page-1' },
+        { text: 'sample 2', link: 'en/technology-design/page-2' }
+      ],
+            'Insights': [
+        { text: 'sample 3', link: 'en/technology-design/page-3' },
+        { text: 'sample 4', link: 'en/technology-design/page-4' }
+      ],
+    },
+    'Society & Culture': {
+      'Sparks': [
+        { text: 'sample 1', link: 'en/society-culture/page-1' },
+        { text: 'sample 2', link: 'en/society-culture/page-2' }
+      ],
+            'Insights': [
+        { text: 'sample 3', link: 'en/society-culture/page-3' },
+        { text: 'sample 4', link: 'en/society-culture/page-4' }
+      ],
+    }
+        
   }
 }
